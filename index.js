32b9dbc36bcfeedf79d7db6ee58de87a9ff38003
OmarSuhendro <ows131174@gmail.com>
2024-09-14 16:22:45 +0700

    update sore

---
+++ index.js
@@ -0,0 +1,18 @@
+var cars = "Avanza, Mobilio, Ayla, Brio, Jazz"
+console.log(cars)
+//document.writeln(cars)
+
+var gretting = "Hello World"
+console.log(gretting)
+//document.writeln(gretting)
+
+var dataString = "Ini String"
+var dataNumber = 1234567
+var dataBoolean = true //false
+var a = 20
+var b = 30
+var result = a*b
+
+console.log(dataString)
+console.log(dataNumber)
+console.log(dataBoolean)
